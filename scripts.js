46e81af63b8c789cfd2eabf420e4796384448aea
GelsonDs <gelsondantasitb@gmail.com>
2024-07-21 15:55:28 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let formulario = document.querySelector(".formulario")
+
+console.log(formulario)
+
+function apareceforulario(){
+    formulario.style.left = "50%"
+
+}
+
+function desapareceforulario(){
+
+    formulario.style.left = "-50%"
+
+
+}
